a54d3bb9a5f98f3438afb1303965810ddbdac4ea
Norvo <68480024+Norvo@users.noreply.github.com>
2021-11-08 19:27:34 +0100

    As I thought it doesnt work but oh well

--- zvoncek/process.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$json_string = json_encode($_POST);
-$menuPerArray = "text/jsonmenutext.json";
-$fileHandle = fopen($menuPerArray, "w");
-fwrite($fileHandle, $json_string);
-fclose($fileHandle);
-
-
